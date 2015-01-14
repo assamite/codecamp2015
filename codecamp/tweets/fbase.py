736afe0d9ed838b083f02f7e0d1be7c1b8ec1bbc
Simo Linkola <slinkola@cs.helsinki.fi>
2015-01-14 17:06:30 +0200

    Changed freebase.py to fbase.py and added views.

--- codecamp/tweets/fbase.py
+++ codecamp/tweets/fbase.py
@@ -19,12 +19,12 @@ def query(id = None, name = None, type = None):
     if id is None and name is None and type is None:
         raise Exception("You have to specify at least on of the following: id, name or type")
     
-    api_key = open(settings.FREEBASE_API_KEY).read()
+    #api_key = open(settings.FREEBASE_API_KEY).read()
     service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
     query = [{'id': id, 'name': name, 'type': type}]
     params = {
         'query': json.dumps(query),
-        'key': api_key
+        #'key': api_key
     }
     url = service_url + '?' + urllib.urlencode(params)
     response = json.loads(urllib.urlopen(url).read())
