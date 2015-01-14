6f06bcdf53449aa98fd3e2e8ff55a79871f8bca9
Simon Cutajar <scutajar@gmail.com>
2015-01-14 15:44:58 +0000

    Added fetch movies from IMDB method in io.py

--- codecamp/tweets/io.py
+++ codecamp/tweets/io.py
@@ -8,6 +8,7 @@ import sys
 import os
 import logging
 import traceback
+import math
 from models import Article
 from models import Movie
 
@@ -23,6 +24,16 @@ import tweepy
 logger = logging.getLogger('tweets.default')
 
 DEBUG = False
+
+def download_en(url, cached=False): # set cached=True to store the download source locally (faster)
+    id = "en-hack-" + url
+    if cached and url in cache:
+        return cache[id]
+    r = urllib2.Request(encode_utf8(url), "get", {"Accept-Language": "en-US"})
+    src = urllib2.urlopen(r).read()
+    src = decode_utf8(src) # Unicode
+    cache[id] = src
+    return src
 
 def tweet(self, tweet):
     '''Tweet to the twitter.
@@ -51,8 +62,6 @@ def tweet(self, tweet):
     
     return (True, tweet)
 
-
-
 def fetch_articles_from_web(self, count):
     articles = []
     for i in range(0,count):
@@ -64,32 +73,95 @@ def fetch_articles_from_web(self, count):
         
     return articles
 
-
-
-def fetch_movies_from_web(self, count):
+def fetch_movies_from_web(self, amount):
     movies = []
-    for i in range(0,count):
-        #Fetch movies from imdb
-        #Create Movie object
-        #Add to list
-        a = Movie() 
-        movies.append(a)
+    mainUrl = "http://www.imdb.com/search/title?groups=top_1000&sort=user_rating&view=simple"
+    listPos = 1;
+    newMovie = Movie()
+    newPerson = Person()
+    newCast = Cast()
+    newKeyword = Keyword()
+    
+    totalResults = 0;
+
+    pageAmount = int(math.ceil(amount / float(100)))
+
+    for i in range(0, pageAmount):
+        url = mainUrl + "&start=" + str(listPos)
+        listPos += 100
+        src = URL(url).download(cached = True)
+        dom = DOM(src)
+
+        ranking = dom(".number") #get all rankings
+		count = 0
+		
+        for p in dom(".title a"):
+            if totalResults == amount:
+                break
+
+            movieURL = "http://akas.imdb.com" + p.href
+
+            src = download_en(movieURL) #force english downloading of movie URL
+            filmDom = DOM(src)
+
+            yearNumber = 0
+            
+            newMovie.toplist_pos = ranking[count][0]
+            newMovie.url = movieURL
+
+            #get film year
+            for filmYear in filmDom(".header .nobr"):
+                yearNumber = filmYear[1][0]
+                newMovie.year = yearNumber
+
+            #get film title
+            for filmTitle in filmDom(".header .itemprop"):
+                newMovie.title = filmTitle[0]
+
+            #get film genres
+            for filmGenre in filmDom(".infobar .itemprop"):
+				newMovie.genre = filmGenre[0]
+                
+            #intentionally commented out for now
+
+            #get film keywords
+            #for filmKeyword in filmDom(".see-more .itemprop"):
+			#	newKeyword.word = filmKeyword[0]
+            #    newKeyword.save() #add new keyword
+            #    newMovie.add(newKeyword)
+
+            #get film actors
+            for filmActor in filmDom(".cast_list .itemprop a span"):
+				newCast.name = filmActor[0]
+				newCast.save()
+				newMovie.append(newCast)
+				print filmActor[0]
+
+            #get film characters
+            for filmCharacter in filmDom(".cast_list .character a"):
+				newPerson.name = filmCharacter[0]
+				newPerson.save()
+				newMovie.append(newPerson)
+                print filmCharacter[0]
+
+            #get short summary
+            filmShortSummary = filmDom("#overview-top p")[1]
+            filmShortSummary = str(filmShortSummary)
+            filmShortSummary = filmShortSummary.replace('<p itemprop="description">', "")
+            filmShortSummary = filmShortSummary.replace('</p>', "")
+            filmShortSummary = filmShortSummary.strip()
+            newMovie.short_summary = filmShortSummary
+
+            #get long summary
+            for filmLongSummary in filmDom("#titleStoryLine .canwrap p"):
+                filmLongSummary = filmLongSummary[0]
+                filmLongSummary = str(filmLongSummary).strip()
+                newMovie.long_summary = filmLongSummary
+
+            count += 1
+            totalResults += 1
+			
+			newMovie.save()
+			movies.append(newMovie)
         
     return movies
-
-
-
-def fetch_movies_from_database(self):
-    movies = []
-    #Query DB and store movies in list. Return list.
-    
-    return movies
-
-
-
-def push_movies_to_database(movies):
-    
-    #Push list of movies to database
-    
-    return 1
-
